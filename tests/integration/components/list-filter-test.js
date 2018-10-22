f62f867bb46339157e41c0daab9603b8ee9a3c69
Rodolfo Valdivieso <rodolfo@assetpanda.com>
2018-10-22 11:28:17 -0500

    first commit

---
+++ tests/integration/components/list-filter-test.js
@@ -0,0 +1,48 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, settled, triggerKeyEvent, fillIn } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import { resolve } from 'rsvp';
+
+const ITEMS = [{city: 'San Francisco'}, {city: 'Portland'}, {city: 'Seattle'}];
+const FILTERED_ITEMS = [{city: 'San Francisco'}];
+
+module('Integration | Component | list-filter', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('should update with matching listings', async function (assert) {
+  this.set('filterByCity', (val) =>  {
+    if (val === '') {
+      return resolve({
+        query: val,
+        results: ITEMS });
+    } else {
+      return resolve({
+        query: val,
+        results: FILTERED_ITEMS });
+    }
+  });
+
+  await render(hbs`
+    {{#list-filter filter=(action filterByCity) as |results|}}
+      <ul>
+      {{#each results as |item|}}
+        <li class="city">
+          {{item.city}}
+        </li>
+      {{/each}}
+      </ul>
+    {{/list-filter}}
+  `);
+
+  // filling in the component's input field with 's'
+  await fillIn(this.element.querySelector('.list-filter input'),'s');
+  // The keyup event here should invoke an action that will cause the list to be filtered
+  await triggerKeyEvent(this.element.querySelector('.list-filter input'), "keyup", 83);
+
+  return settled().then(() => {
+    assert.equal(this.element.querySelectorAll('.city').length, 1, 'One result returned');
+    assert.equal(this.element.querySelector('.city').textContent.trim(), 'San Francisco');
+  });
+});
+});
